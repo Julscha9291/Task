4a8b0c7c36e137c3325b3b424cacfa4df1547249
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-17 14:57:17 +0200

    url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home'), 
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),  # Registriere deine tasks-URLs
 ]
