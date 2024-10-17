f36c94414c31fb50e9a8384c2dc6d8f6d6c83aa0
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-17 13:58:54 +0200

    statics

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -197,8 +197,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# List of additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/public',
+]
+
+# Absolute filesystem path to the directory that will hold static files.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
