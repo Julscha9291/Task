14b9a0310cc0ca6f91c2347970cb9c89eb1e8152
Julscha9291 <j.schaepermeier91@gmail.com>
2024-10-17 14:52:56 +0200

    urlsd

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  # Importiere die Umleitungsfunktion
+
+# Umleitung zur API oder einer spezifischen Seite
+def redirect_to_api(request):
+    return redirect('api/')  # Hier kannst du die gewünschte Ziel-URL angeben
 
 urlpatterns = [
+    path('', redirect_to_api, name='redirect_to_api'),  # Definiere die Root-URL
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls'))
+    path('api/', include('tasks.urls')),  # Registriere deine tasks-URLs
 ]
 
 if settings.DEBUG:
